a3ed02d30fd057c44f11eed6698e7b25d7c21671
Bastien Mourguiart <bastien.mourguiart@gmail.com>
2021-11-29 17:06:35 +0100

    initial version after the first day

---
+++ exercices/exo_dplyr.R
@@ -0,0 +1,20 @@
+#####################################%
+# Formation CESAB, Reproducitbilite
+# 29/11/21
+#
+# eco_dplyr.R
+#
+# Script pour l'exo dplyr
+# Faire un hist du nb de mams par ecoregions
+#####################################%
+
+
+# load sp-eco data
+
+dat <- read_eco_sp()
+
+mam_per_eco <- table(dat$ecoregion_id)
+
+png(filename = here::here("outputs", "exo_dplyr_hist_mams.png"))
+hist(mam_per_eco, breaks = 50)
+dev.off()
---
+++ make.R
@@ -0,0 +1,20 @@
+#################################################################%
+# Formation CESAB, Reproducitbilite
+# 29/11/21
+#
+# make.R
+#
+# Script pour l'execution du projet entier (automatisation)
+#################################################################%
+
+
+# install dependences (packages used)
+devtools::install_deps()
+
+# load functions
+# devtools::load_all() # but does not work with package 'targets'
+source(here::here("R", "data_wildfinder.R"))
+
+# run exodplyr
+source(here::here("exercices", "exo_dplyr.R"))
+# lala
