050e810245edbb5703b3e297ccee971ffc6a9a5e
Bastien Mourguiart <bastien.mourguiart@gmail.com>
2021-11-30 09:36:17 +0100

    Precise xlab

--- exercices/exo_dplyr.R
+++ exercices/exo_dplyr.R
@@ -18,7 +18,7 @@ mam_per_eco <- table(dat$ecoregion_id)
 png(filename = here::here("outputs", "exo_dplyr_hist_mams.png"))
 hist(mam_per_eco,
      breaks = 40,
-     xlab = "Nombre de mammiferes",
+     xlab = "Nombre d'epeces de mammiferes",
      ylab = "Nombre d'ecoregions",
      col = "red")
 dev.off()
