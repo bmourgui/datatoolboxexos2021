317f31c457f2a0e8ef6cc2e277c6188aa64002ae
Bastien Mourguiart <bastien.mourguiart@gmail.com>
2021-11-30 12:26:35 +0100

    Make exo 2 dplyr

--- exercices/exo_dplyr.R
+++ exercices/exo_dplyr.R
@@ -8,7 +8,6 @@
 # Faire un hist du nb de mams par ecoregions
 ############################################%
 
-
 # load sp-eco data
 
 dat <- read_eco_sp()
@@ -22,3 +21,37 @@ hist(mam_per_eco,
      ylab = "Nombre d'ecoregions",
      col = "red")
 dev.off()
+
+
+# Exo 2
+data_eco_sp <- read_eco_sp()
+data_eco_list <- read_eco_list()
+data_sp_list <- read_sp_list()
+
+data_eco_sp %>%
+  dplyr::inner_join(data_eco_list) %>%
+  dplyr::inner_join(data_sp_list) -> data
+
+data %>%
+  dplyr::filter(family == "Ursidae", sci_name != "Ursinus malayanus") %>%
+  dplyr::select(ecoregion, realm, biome, sci_name) -> data_urs
+
+data_urs %>%
+  dplyr::count(ecoregion, sci_name) %>%
+  dplyr::group_by(sci_name) %>%
+  dplyr::summarise(n_ecoregion = dplyr::n_distinct(ecoregion)) -> data_n_ecor
+
+data_urs %>%
+  dplyr::count(biome, sci_name) %>%
+  dplyr::group_by(sci_name) %>%
+  dplyr::summarise(n_biome = dplyr::n_distinct(biome)) -> data_n_biome
+
+data_urs %>%
+  dplyr::count(realm, sci_name) %>%
+  dplyr::group_by(sci_name) %>%
+  dplyr::summarise(n_realm = dplyr::n_distinct(realm)) -> data_n_realm
+
+data_n_realm %>%
+  dplyr::left_join(data_n_biome) %>%
+  dplyr::left_join(data_n_ecor)
+
